e79ea4b61e4607281580a0bccc61bb3226b6c777
soni801 <32430506+soni801@users.noreply.github.com>
2020-11-15 16:18:23 +0100

    Add graphics

--- script.js
+++ script.js
@@ -4,18 +4,15 @@ window.onscroll = function() {scroll()};
 // Get the navbar
 const navbar = document.getElementById("navbar");
 
+// Set screen-space correctly
+document.getElementById("screen-space").style.minHeight = (window.innerHeight - navbar.offsetHeight).toString() + "px";
+
 // Get the offset position of the navbar
 const sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function scroll()
 {
-    if (window.pageYOffset >= sticky)
-    {
-        navbar.classList.add("sticky")
-    }
-    else
-    {
-        navbar.classList.remove("sticky");
-    }
+    if (window.pageYOffset >= sticky) navbar.classList.add("sticky");
+    else navbar.classList.remove("sticky");
 }
